008f27cb59a2adae97e1447c87caf6134efbdcf4
Kamil Andrzejewski <kamil.j.andrzejewski@student.put.poznan.pl>
2019-11-05 22:13:24 +0100

    Projekt pierwotny

---
+++ SNMP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Regex_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regEmail = new Regex(@"^[a-z][a-z0-9_.-kam-]*@[a-z0-9]*\.[a-z]{2,3}$");
+            ReadFile ReadMIB = new ReadFile();
+            string napis;
+            ReadMIB.ReadFromFile(@"Plik", @"..\data");
+            napis = ReadMIB.sLine;
+            Console.WriteLine(
+                String.Format("{0} podany adres to {1}poprawny adres email", napis,
+                                regEmail.IsMatch(napis) ? "" : "nie"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SNMP/ReadFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Regex_example
+{
+    public class ReadFile
+    {
+        public string sLine;
+
+        public void ReadFromFile(string sFileName, string sPath) {
+            try
+            {
+                var sLines = File.ReadLines(sPath + @"\"+ sFileName + @".txt", Encoding.UTF8);
+                foreach (var line in sLines)
+                {
+                    sLine = line;
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Nie można otworzyć pliku:");
+                Console.WriteLine(e.Message);
+            }
+           
+        }
+    }
+}
