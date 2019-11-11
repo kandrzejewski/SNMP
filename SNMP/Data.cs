06758623a05a7c6f2db46d2665abb1ea7523f5c1
Kamil Andrzjewski <kamil.j.andrzejewski@student.put.poznan.pl>
2019-11-11 13:50:02 +0100

    Dodano wywoływanie kolejnych plików.

---
+++ SNMP/Data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_example
+{
+    class Data
+    {
+        public int id;
+        public string email;
+        public string country;
+        public string title;
+        public string name;
+
+        public void WriteData()
+        {
+            Console.WriteLine("------------");
+            Console.WriteLine(id);
+            Console.WriteLine(email);
+            Console.WriteLine(country);
+            Console.WriteLine(title);
+            Console.WriteLine(name);
+            Console.WriteLine("------------");
+        }
+
+        public Data oSubData = new Data();
+    }
+}
--- SNMP/Pharse.cs
+++ SNMP/Pharse.cs
@@ -14,26 +14,60 @@ namespace Regex_example
         {
             RegexOptions options = RegexOptions.Singleline & RegexOptions.Compiled;
             Regex regEmail = new Regex(@"{Person:\s*(?<Person>[1-9])}\s*e-mail:\s*" + 
-                                        @"(?<email>[a-z][a-z0-9_.-kam-]*@[a-z0-9]*\.[a-z]{2,3})\s*{[0-9]}\s*Country\s*-\s*" + 
+                                        @"(?<email>[a-z][a-z0-9_.-]*@[a-z0-9]*\.[a-z]{2,3})\s*{[0-9]}\s*Country\s*-\s*" + 
                                         @"(?<country>\w*)\s*{[1-9]}\s*title:\s*" + 
                                         @"(?<title>.*?)\s*{[1-9]}\s*name:\s*" + 
                                         @"(?<name>\w*\s*\w*)\s*{[1-9]}", options);
-            Regex regNewFile = new Regex(@"Plik2");
+            Regex regNewFile = new Regex(@"openfile\s*-\s*"+"\""+ @"(?<name>.*?)" + "\""+@"\s*");
 
             MatchCollection matches = regEmail.Matches(_text);
+            MatchCollection matchesimport = regNewFile.Matches(_text);
+
             foreach (Match _match in matches)
             {
+                Data oData = new Data();
                 for(int _iGroupNumber = 1; _iGroupNumber < _match.Groups.Count; _iGroupNumber++)
                 {
-                    Console.WriteLine("Group {0}: {1}", regEmail.GroupNameFromNumber(_iGroupNumber), _match.Groups[_iGroupNumber].Value);
+                    //Console.WriteLine("Group {0}: {1}", regEmail.GroupNameFromNumber(_iGroupNumber), _match.Groups[_iGroupNumber].Value);
+                    switch (_iGroupNumber)
+                    {
+                        case 1:
+                            {
+                                oData.id = Int32.Parse(_match.Groups[_iGroupNumber].Value);
+                                continue;
+                            }
+                        case 2:
+                            {
+                                oData.email = _match.Groups[_iGroupNumber].Value;
+                                continue;
+                            }
+                        case 3:
+                            {
+                                oData.country = _match.Groups[_iGroupNumber].Value;
+                                continue;
+                            }
+                        case 4:
+                            {
+                                oData.title = _match.Groups[_iGroupNumber].Value;
+                                continue;
+                            }
+                        case 5:
+                            {
+                                oData.name = _match.Groups[_iGroupNumber].Value;
+                                continue;
+                            }
+                    }
                 }
+                oData.WriteData();
             }
 
             if (regNewFile.IsMatch(_text))
             {
                 Console.WriteLine(@"Wystąpiło wywołanie 2 pliku");
+                Match _match = matchesimport[0];
+                Console.WriteLine(_match.Groups[1].Value);
                 ReadFile NewFile = new ReadFile();
-                NewFile.ReadFromFile(_text);
+                NewFile.ReadFromFile(_match.Groups[1].Value);
             }
         }
     }
